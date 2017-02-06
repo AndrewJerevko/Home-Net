f10c21e0671dce6db12ef24280df69e73801135c
Andrey Jerevko <andrey.jerevko@gmail.com>
2017-02-06 15:53:28 +0200

    Подключил Ajax исправил проблему с пропадающим списком v0.12

--- js/func.js
+++ js/func.js
@@ -1,30 +1,69 @@
 $(document).ready(function () {
+  $('#btn1').click(function () {
+    $(".news").load("../index2.html .1");
+  });
+    $('#btn2').click(function () {
+    $(".news").load("../index2.html .2");
+  });
+    $('#btn3').click(function () {
+    $(".news").load("../index2.html .3");
+  });
+    $('#btn4').click(function () {
+    $(".news").load("../index2.html .4");
+  });
+    $('#btn5').click(function () {
+    $(".news").load("../index2.html .5");
+  });
+    $('#btn6').click(function () {
+    $(".news").load("../index2.html .6");
+  });
+    $('#btn7').click(function () {
+    $(".news").load("../index2.html .7");
+  });
+    $('#btn8').click(function () {
+    $(".news").load("../index2.html .8");
+  });
+    $('#btn9').click(function () {
+    $(".news").load("../index2.html .9");
+  });
+    $('#btn10').click(function () {
+    $(".news").load("../index2.html .10");
+  });
+    $('#btn11').click(function () {
+    $(".news").load("../index2.html .11");
+  });
+
+  $(".ulli").click(function () {
+    $(".ulli").show();
+    return false;
+  });
+
   $(".box").click(function () {
     $(".inbox").fadeToggle("slow");
-    $(".inbox2").hide(3000);
-    $(".inbox3").hide(3000);
-    $(".inbox4").hide(3000);
+    $(".inbox2").hide();
+    $(".inbox3").hide();
+    $(".inbox4").hide();
     return false;
   });
-    $(".box2").click(function () {
+  $(".box2").click(function () {
     $(".inbox2").fadeToggle("slow");
-    $(".inbox").hide(3000);
-    $(".inbox3").hide(3000);
-    $(".inbox4").hide(3000);
+    $(".inbox").hide();
+    $(".inbox3").hide();
+    $(".inbox4").hide();
     return false;
   });
-    $(".box3").click(function () {
+  $(".box3").click(function () {
     $(".inbox3").fadeToggle("slow");
-    $(".inbox").hide(3000);
-    $(".inbox2").hide(3000);
-    $(".inbox4").hide(3000);
+    $(".inbox").hide();
+    $(".inbox2").hide();
+    $(".inbox4").hide();
     return false;
   });
-    $(".box4").click(function () {
+  $(".box4").click(function () {
     $(".inbox4").fadeToggle("slow");
-    $(".inbox").hide(3000);
-    $(".inbox2").hide(3000);
-    $(".inbox3").hide(3000);
+    $(".inbox").hide();
+    $(".inbox2").hide();
+    $(".inbox3").hide();
     return false;
   });
 });

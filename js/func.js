706ffd274e8857535bb11a7e64a28e4575ec178f
Andrey Jerevko <andrey.jerevko@gmail.com>
2017-02-16 12:05:19 +0200

    сдача

--- js/func.js
+++ js/func.js
@@ -2,37 +2,30 @@ $(document).ready(function () {
   $('#btn1').click(function () {
     $(".news").load("../index2.html .1");
   });
-    $('#btn2').click(function () {
+  $('#btn2').click(function () {
     $(".news").load("../index2.html .2");
   });
-    $('#btn3').click(function () {
+  $('#btn3').click(function () {
     $(".news").load("../index2.html .3");
   });
-    $('#btn4').click(function () {
+  $('#btn4').click(function () {
     $(".news").load("../index2.html .4");
   });
-    $('#btn5').click(function () {
+  $('#btn5').click(function () {
     $(".news").load("../index2.html .5");
   });
-    $('#btn6').click(function () {
+  $('#btn6').click(function () {
     $(".news").load("../index2.html .6");
   });
-    $('#btn7').click(function () {
+  $('#btn7').click(function () {
     $(".news").load("../index2.html .7");
   });
-    $('#btn8').click(function () {
+  $('#btn8').click(function () {
     $(".news").load("../index2.html .8");
   });
-    $('#btn9').click(function () {
+  $('#btn9').click(function () {
     $(".news").load("../index2.html .9");
   });
-    $('#btn10').click(function () {
-    $(".news").load("../index2.html .10");
-  });
-    $('#btn11').click(function () {
-    $(".news").load("../index2.html .11");
-  });
-
   $(".ulli").click(function () {
     $(".ulli").show();
     return false;
@@ -60,10 +53,68 @@ $(document).ready(function () {
     return false;
   });
   $(".box4").click(function () {
-    $(".inbox4").fadeToggle("slow");
+  $(".news").load("../index2.html .10");
     $(".inbox").hide();
     $(".inbox2").hide();
     $(".inbox3").hide();
     return false;
   });
+
+  $(".contact").click(function(){
+    $(".news").load("../index2.html #contact")
+    return false;
+  });
+
+  $(".login").click(function () {
+    $("#form")
+      .css('display', 'block')
+      .animate({
+        opacity: 1,
+        top: '-50px'
+      }, 400);
+    $(".login").hide();
+    return false;
+  });
+  $("#form_close").click(function () {
+    $("#form")
+      .css('display', 'none')
+      .animate({
+        opacity: 0,
+        top: '-50px'
+      }, 400);
+    $(".login").show();
+    return false;
+  });
+
+  $("#sub_bot").click(function () {
+    $("#subscribe")
+      .css('display', 'block');
+    $("#login")
+      .css('display', 'none')
+    return false;
+  });
+  $("#login_bot").click(function () {
+    $("#login")
+      .css('display', 'block');
+    $("#subscribe")
+      .css('display', 'none')
+    return false;
+  });
+  (function () {
+    var widget_id = 867427;
+    _shcp = [{
+      widget_id: widget_id
+    }];
+    var lang = (navigator.language || navigator.systemLanguage ||
+        navigator.userLanguage || "en")
+      .substr(0, 2).toLowerCase();
+    var url = "widget.siteheart.com/widget/sh/" + widget_id + "/" + lang + "/widget.js";
+    var hcc = document.createElement("script");
+    hcc.type = "text/javascript";
+    hcc.async = true;
+    hcc.src = ("https:" == document.location.protocol ? "https" : "http") +
+      "://" + url;
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hcc, s.nextSibling);
+  })();
 });
